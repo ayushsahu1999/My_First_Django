236789f2e14af355f5b1932ffb148308184cc333
ayush <ayushsahu99@gmail.com>
2019-09-26 00:06:08 +0530

    5- Challenge 2

--- ProTwo/AppTwo/views.py
+++ ProTwo/AppTwo/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<em>My Second App</em>')
+    my_dict = {'insert_me':'Hello, I completed the challenge!'}
+    return render(request, 'AppTwo/help.html', context=my_dict)
