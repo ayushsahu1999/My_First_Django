b8fcf147b93d24760c43fbac7419b7a1e829df69
ayush <ayushsahu99@gmail.com>
2019-09-28 19:34:20 +0530

    11- Forms Basic

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -21,6 +21,7 @@ from first_app import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^first_app/', include('first_app.urls')),
     path('admin/', admin.site.urls),
+    url(r'^first_app/', include('first_app.urls')),
+    url(r'^formpage/', views.form_name_view, name='form_name'),
 ]
